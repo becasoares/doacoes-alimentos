524777f236069336b4b14af593263f68a76c62b4
Anna Rebeca <souzaannarebeca@gmail.com>
2025-06-18 23:28:19 -0300

    Create home page

---
+++ src/screens/home.jsx
@@ -0,0 +1,27 @@
+function Home () {
+    return (
+        <>
+        <div id="header"></div>
+
+  <section class="hero">
+    <h2>Ajude quem precisa com um gesto simples: doe alimentos!</h2>
+    <p>Transforme solidariedade em ação. Sua doação pode mudar vidas.</p>
+    <a href="#formulario" class="btn">Quero Doar</a>
+  </section>
+
+  <main>
+    <section id="como">
+      <h2>Como funciona</h2>
+      <p>Nosso sistema conecta doadores a instituições e famílias que realmente precisam. Basta preencher o formulário e entraremos em contato.</p>
+      <ul>
+        <li>Preencha o formulário de doação</li>
+        <li>Entramos em contato com você</li>
+        <li>Coletamos e direcionamos os alimentos</li>
+      </ul>
+    </section>
+    </main>
+        </>
+    )
+}
+
+export default Home
